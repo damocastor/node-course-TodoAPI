ab209a6ccf15c9f83de42e8496d0b93839a3f442
David Castor <damocastor@gmail.com>
2017-08-15 21:32:39 +0200

    Add GET todos route

--- server/server.js
+++ server/server.js
@@ -19,7 +19,14 @@ app.post('/todos', (req, res) => {
     }, (error) => {
         res.status(400).send(error)
     })
-    
+})
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos})
+    }, (e) => {
+        res.status(400).send(e)
+    })
 })
 
 app.listen(8080, () => {
